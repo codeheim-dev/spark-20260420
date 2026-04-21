9dad4209806d962f7664f47995291a81affb2483
Piotr "Dreknar" Chudzik <chudzik.piotr@proton.me>
2026-04-21 11:02:01 +0200

    zadanie01

---
+++ shared/code/zadanie01.py
@@ -0,0 +1,9 @@
+"""
+1. Stwórz schemat dla pliku orders.csv i zaczytaj plik korzystając ze schematu
+2. Utwórz własny plik csv, a następnie:
+    - stwórz do niego schemat, upewnij się, że conajmniej jedna kolumna nie może być pusta
+    - sprawdź schemat po załadowaniu danych
+    - spróbuj załadować dane dla utworzonego schematu kiedy w pliku csv jest conajmniej jeden rekord
+        z NULL dla kolumny, która nie może być pusta.
+    - spróbuj załadować pusty plik CSV (tylko nagłówek)
+"""
